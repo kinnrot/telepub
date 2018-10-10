db2128c6b5e9fe49c55d00251aa02f77568bb695
Chen Kinnrot <kinnrot@gmail.com>
2018-10-10 20:49:13 +0300

    subscription testing

--- test/cases/subscription-observer-test.ts
+++ test/cases/subscription-observer-test.ts
@@ -23,16 +23,65 @@ export default class SubscriptionObserverTests extends DOMTestCase {
     }
 
 
-    async "test subscribeToTopic"() {
+    async "test subscribeToTopicPrerendered"() {
         const observer = new SubscriptionsObserver(this.context, this.attributeName)
 
         observer.start()
 
         await this.nextFrame
 
-        console.log(this.calls)
         this.assert.deepEqual(this.calls, [["subscribe", this.findElement("#subId"), this.topic]])
+    }
 
+    async "test subscribeTo2TopicsPrerendered"() {
+        const topicToCheck = "other-topic"
+        const anotherTopicToCheck = "another-topic"
+        this.findElement("#subId").setAttribute(this.attributeName, `${topicToCheck} ${anotherTopicToCheck}`)
+        const observer = new SubscriptionsObserver(this.context, this.attributeName)
         
+        observer.start()
+
+        await this.nextFrame
+
+        this.assert.deepEqual(this.calls, [["subscribe", this.findElement("#subId"), topicToCheck], ["subscribe", this.findElement("#subId"), anotherTopicToCheck]])
+    }
+
+    async "test subscribeToTopicAddedViewJs"() {
+        const observer = new SubscriptionsObserver(this.context, this.attributeName)
+        const topicToCheck = "other-topic"
+        this.rootElement.removeAttribute(this.attributeName)
+
+        observer.start()
+
+        await this.nextFrame
+
+        this.findElement("#subId").setAttribute(this.attributeName, topicToCheck)
+
+        await this.nextFrame
+
+        console.log(this.calls)
+        this.assert.deepEqual(this.calls, [["subscribe", this.findElement("#subId"), topicToCheck]])
+    }
+
+    async "test subscribeTo2TopicsAddedViewJs"() {
+        const observer = new SubscriptionsObserver(this.context, this.attributeName)
+        const topicToCheck = "other-topic"
+        const anotherTopicToCheck = "another-topic"
+        this.rootElement.removeAttribute(this.attributeName)
+
+        observer.start()
+
+        await this.nextFrame
+
+        this.findElement("#subId").setAttribute(this.attributeName, `${topicToCheck} ${anotherTopicToCheck}`)
+
+        await this.nextFrame
+
+        console.log(this.calls)
+        this.assert.deepEqual(this.calls, [["subscribe", this.findElement("#subId"), topicToCheck], ["subscribe", this.findElement("#subId"), anotherTopicToCheck]])
+    }
+
+    get rootElement() {
+        return this.findElement("#subId")
     }
 }
