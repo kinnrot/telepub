5bbc60576ce536d97bc0becac36d1bb00e7fb937
Chen Kinnrot <kinnrot@gmail.com>
2018-10-15 09:07:40 +0300

    added event fire and suscribers notifications on observe

--- src/core/live_data.ts
+++ src/core/live_data.ts
@@ -1,24 +1,21 @@
 export class LiveData {
     constructor(private value: string, private attributeName: string) {
         this.observers = new Array<Element>()
+        this.oldValue = ""
     }
 
+    oldValue: string
     observers: Element[]
 
     postValue(newValue: string) {
         this.setValue(newValue)
     }
-
-    setValue(newValue: string) {
-        if (this.value !== newValue) {
-            const oldValue = this.value
-            this.value = newValue
-            this.notifySubscribers()
-        }
-    }
-
+    
     observe(element: Element) {
         this.observers.push(element)
+
+        // when we observe data, we should get the data initial value immidietly
+        this.updateSubscriber(element)
     }
 
     removeObserver(element: Element) {
@@ -28,12 +25,30 @@ export class LiveData {
         }
     }
 
-    async notifySubscribers() {
+    private setValue(newValue: string) {
+        if (this.value !== newValue) {
+            this.oldValue = this.value
+            this.value = newValue
+            this.notifySubscribers()
+        }
+    }
+
+
+    private async notifySubscribers() {
         // make sure we only post last value for each event loop iteration
         await setTimeout(() => {
             this.observers.forEach(element => {
-                element.setAttribute(this.attributeName, this.value)
+                this.updateSubscriber(element)
             })
         }, 0)
+    }
+
+    private async updateSubscriber(element: Element) {
+        if (element.getAttribute(this.attributeName) !== this.value) {
+            element.setAttribute(this.attributeName, this.value)
+            
+            const event = new CustomEvent(`${this.attributeName}-changed`, { detail: { oldValue: this.oldValue, newValue: this.value } })
+            element.dispatchEvent(event)
+        }
     }
 }
